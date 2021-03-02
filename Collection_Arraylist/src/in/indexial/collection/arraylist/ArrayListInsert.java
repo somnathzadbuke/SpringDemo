111c2e7266f1218b687d59a7c641455d95693719
somnathzadbuke <somzadbuke@gmail.com>
2021-03-02 14:09:30 +0530

    aaaa

---
+++ Collection_Arraylist/src/in/indexial/collection/arraylist/ArrayListInsert.java
@@ -0,0 +1,21 @@
+package in.indexial.collection.arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListInsert {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		
+		al.add(13);
+		al.add(25);
+		al.add(null);
+		al.add(12);
+		System.out.println(al);
+		
+		
+
+	}
+
+}
