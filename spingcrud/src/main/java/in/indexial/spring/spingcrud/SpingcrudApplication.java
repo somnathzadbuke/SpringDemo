d3253bcba3e3e47ebb177b2a8b3bdcb93bbe137a
somnathzadbuke <somzadbuke@gmail.com>
2021-03-02 13:58:30 +0530

    First Commit

---
+++ spingcrud/src/main/java/in/indexial/spring/spingcrud/SpingcrudApplication.java
@@ -0,0 +1,13 @@
+package in.indexial.spring.spingcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingcrudApplication.class, args);
+	}
+
+}
---
+++ spingcrud/src/main/java/in/indexial/spring/spingcrud/dao/EmployeeDao.java
@@ -0,0 +1,7 @@
+package in.indexial.spring.spingcrud.dao;
+
+public class EmployeeDao implements DataJPA {
+ 
+	
+	
+}
